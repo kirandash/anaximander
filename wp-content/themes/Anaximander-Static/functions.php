b8cd57c81fbfd5d5d4da31cb874ceb8332bbf296
kiran kumar dash <thekirankumardash@gmail.com>
2015-08-26 17:47:25 +0530

    Make videosresponsive by including fitsvid.js.

--- wp-content/themes/Anaximander-Static/functions.php
+++ wp-content/themes/Anaximander-Static/functions.php
@@ -558,6 +558,66 @@ add_action('init', 'anaximander_superfish');
 
 
 
+
+// Add FitVids to allow for responsive sizing of videos
+
+function anaximander_fitvids() {
+
+	if (!is_admin()) {
+
+
+
+		wp_register_script( 'fitvids', get_template_directory_uri() . '/js/jquery.fitvids.js', array('jquery'), '1.0', true);    	
+
+    	wp_enqueue_script( 'fitvids');
+
+    	add_action('wp_head', 'add_fitthem');
+
+    	
+
+    	function add_fitthem() { ?>
+
+	    	<script type="text/javascript">
+
+		    	jQuery(document).ready(function() {
+
+	    			jQuery('.video').fitVids();
+
+	    		});
+
+    		</script><?php
+
+	    }
+
+
+
+	}
+
+}
+
+
+
+add_action('init', 'anaximander_fitvids');
+
+
+
+
+
+// Automatically append .video class to oembed content (not a perfect solution, but close)
+
+function anaximander_embed_filter( $html, $data, $url ) {
+
+
+
+	$return = '<div class="video">'.$html.'</div>';
+
+	return $return;
+
+}
+
+add_filter('oembed_dataparse', 'anaximander_embed_filter', 90, 3 );
+
+
 /**
  * The End
  */
